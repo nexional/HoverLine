db76ca1f318a743c789c625155f7cfda41b6492b
nexional <19317423+nexional@users.noreply.github.com>
2022-07-24 04:53:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+```
+ __   __ _______ __   __ _______ ______   ___     ___ __    _ _______ 
+|  | |  |       |  | |  |       |    _ | |   |   |   |  |  | |       |
+|  |_|  |   _   |  |_|  |    ___|   | || |   |   |   |   |_| |    ___|
+|       |  | |  |       |   |___|   |_||_|   |   |   |       |   |___ 
+|       |  |_|  |       |    ___|    __  |   |___|   |  _    |    ___|
+|   _   |       ||     ||   |___|   |  | |       |   | | |   |   |___ 
+|__| |__|_______| |___| |_______|___|  |_|_______|___|_|  |__|_______|
+
+```
+# HoverLine
+
+Ever wondered if there was a way to see the a line in its entirety without scrolling to right or turning word-wrap on & off again. Specially when the file has long lines and you prefer
+word-wrap off for better readability, say for log files.
+
+**HoverLine** can help you there. This package shows the hovered-over line in its entirety as a tooltip, given:
+
+* word-wrap is off for the current view
+* the line has some part hidden on either side (gutter or right side)
+
+If you have word-wrap on for the view or no part of the line is hidden then there's no tooltip.
+
+## Installation
+
+* Install [Sublime Text Package Control](https://packagecontrol.io). Skip if installed already
+* Go to _Tools > Command Palette_. Select `Package Control: Install Package`
+* Type or select `HoverLine` and hit Enter
+* Wait for installation to finish
+
+## How to use
+
+Just hover over the line which is not fully visible. It'll show a tooltip showing entire line,
+wrapped within given space. By default the tooltip is shown for 3 seconds which is configurable.
+
+Tooltip can be disabled if desired (see **Customization**)
+
+## Customization
+
+You can override the default settings in two ways:
+
+* From _Command Palette_ you can run following commands:
+    * `HoverLine: toggle tooltip`
+    * `HoverLine: configure tooltip timeout`
+
+* User Settings file (_Preferences > Package Settings > HoverLine > Settings_):
+```
+{
+    // Enable/disable the package
+    "enable": true
+
+    // Tooltip timeout in seconds
+    "tooltip_timeout": 3
+}
+```
+
+## License
+
+[GNU General Public License v3.0](https://github.com/nexional/HoverLine/blob/master/LICENSE)
+
+## Issues
+
+Please report any bugs/issues [here](https://github.com/nexional/HoverLine/issues/new)
+
+## My other work
+
+* [CompareBuff](https://packagecontrol.io/packages/CompareBuff)
+* [ConvertEpochToDate](https://packagecontrol.io/packages/ConvertEpochToDate)
---
+++ hover_line.py
@@ -0,0 +1,60 @@
+import sublime
+import sublime_plugin
+import re
+import math
+import html
+from textwrap import wrap
+this_package = 'HoverLine: '
+
+def plugin_loaded():
+    global settings
+    settings = sublime.load_settings('HoverLine.sublime-settings')
+    settings.clear_on_change('enable')
+    settings.add_on_change('enable', HoverLineToolTipCommand.is_enabled())
+
+class HoverLineCommand(sublime_plugin.WindowCommand):
+    def run(self, toggle_package=False, set_tooltip_timeout=False):
+        global settings
+        w = self.window
+        if True not in locals().values(): return
+        if locals()['toggle_package']:
+            settings.clear_on_change('enable')
+            settings.set('enable', not settings.get('enable'))
+            settings.add_on_change('enable', HoverLineToolTipCommand.is_enabled())
+            sublime.save_settings('HoverLine.sublime-settings')
+            sublime.message_dialog(this_package + 'package now ' + ('enabled' if settings.get('enable') else 'disabled'))
+        elif locals()['set_tooltip_timeout']:
+            tooltip_timeout = str(settings.get('tooltip_timeout'))
+            if not tooltip_timeout: tooltip_timeout = '3'
+            def on_done(num):
+                if num and num.isdigit() and int(num) > 0:
+                    settings.set('tooltip_timeout', int(num))
+                    sublime.save_settings('HoverLine.sublime-settings')
+                    sublime.message_dialog(this_package + 'tooltip_timeout updated to ' + str(num))
+                else:
+                    sublime.error_message(this_package + 'please provide a valid whole number')
+                    w.show_input_panel('tooltip_timeout:', num, on_done, None, None)
+            w.show_input_panel('tooltip_timeout:', tooltip_timeout, on_done, None, None)
+        return True
+
+class HoverLineToolTipCommand(sublime_plugin.ViewEventListener):
+    def on_hover(self, point, hover_zone):
+        global settings
+        view = self.view
+        if not self.is_enabled() or view.settings().get('word_wrap'): return
+        row = view.rowcol(point)[0]
+        max_width = math.floor(view.viewport_extent()[0])
+        wrap_len = math.floor(max_width / view.em_width())
+        content = view.substr(view.line(view.text_point(row, 0)))
+        first_visible_col = math.floor(view.viewport_position()[0] / view.em_width())
+        if wrap_len >= len(content) and first_visible_col == 0: return
+        clist = wrap(text=content, width=wrap_len-2, replace_whitespace=False, drop_whitespace=False, break_long_words=True, tabsize=view.settings().get('tab_size'))
+        content = '<div>' + '<br>'.join(re.sub(r'\s', '&nbsp;', html.escape(x)) for x in clist) + '</div>'
+        view.show_popup(content=content, location=view.text_point(row, first_visible_col + 1), max_width=max_width, max_height=len(content))
+        sublime.set_timeout_async(lambda: view.hide_popup(), settings.get('tooltip_timeout') * 1000)
+
+    def is_enabled(self=None):
+        global settings
+        return settings.get('enable')
+
+
